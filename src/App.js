5e052498e0ca0ada173a28956760257ef581f057
aungkhant611658 <aungkhant611658@gmail.com>
2024-03-03 21:13:24 +0630

    setup route link

--- src/App.js
+++ src/App.js
@@ -6,31 +6,27 @@ import New from "./pages/new/New";
 
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <BrowserRouter>
-        <Routes>
-          <Route path="/">
-            <Route index element={<Home />} />
-            <Route path="login" element={<Login />} />
+const App = () => (
+  <div className="App">
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="login" element={<Login />} />
 
-            <Route path="users">
-              <Route index element={<List />} />
-              <Route path=":userId" element={<Single />} />
-              <Route path="new" element={<New />} />
-            </Route>
+        <Route path="users">
+          <Route index element={<List />} />
+          <Route path=":userId" element={<Single />} />
+          <Route path="new" element={<New />} />
+        </Route>
 
-            <Route path="products">
-              <Route index element={<List />} />
-              <Route path=":productId" element={<Single />} />
-              <Route path="new" element={<New />} />
-            </Route>
-          </Route>
-        </Routes>
-      </BrowserRouter>
-    </div>
-  );
-}
+        <Route path="products">
+          <Route index element={<List />} />
+          <Route path=":productId" element={<Single />} />
+          <Route path="new" element={<New />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  </div>
+);
 
 export default App;
