a167db3aceedb4a930edde743d31883ae67e0bd7
aungkhant611658 <aungkhant611658@gmail.com>
2024-03-06 21:17:23 +0630

    setup home page

--- src/components/widget/Widget.jsx
+++ src/components/widget/Widget.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import Box from "@mui/material/Box";
+import KeyboardArrowUpIcon from "@mui/icons-material/KeyboardArrowUp";
 
 import "./Widget.scss";
+import { KeyboardArrowDown } from "@mui/icons-material";
 
-const Widget = () => {
+const Widget = ({ type, icon, link, isMoney, amount, percentage }) => {
   return (
     <Box
       className="widget"
@@ -14,8 +16,46 @@ const Widget = () => {
         justifyContent: "space-between",
       }}
     >
-      <div className="left">Left</div>
-      <div className="right">Right</div>
+      <Box
+        className="left"
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          flexWrap: "nowrap",
+          justifyContent: "space-between",
+        }}
+      >
+        <span className="title">{type}</span>
+        <span className="counter">
+          {isMoney && "$"} {amount}
+        </span>
+        <span className="link">{link}</span>
+      </Box>
+
+      <Box
+        className="right"
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          flexWrap: "nowrap",
+          justifyContent: "space-between",
+        }}
+      >
+        <Box
+          className={`percentage ${
+            Math.sign(percentage) == 1 ? "positive" : "negative"
+          }`}
+          sx={{
+            display: "flex",
+            alignItems: "center",
+          }}
+        >
+          {Math.sign(percentage) == 1 && <KeyboardArrowUpIcon />}
+          {Math.sign(percentage) == -1 && <KeyboardArrowDown />}
+          {percentage}%
+        </Box>
+        {icon}
+      </Box>
     </Box>
   );
 };
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -6,8 +6,14 @@ import CssBaseline from "@mui/material/CssBaseline";
 import Navbar from "../../components/navbar/Navbar";
 import Sidebar from "../../components/sidebar/Sidebar";
 import Widget from "../../components/widget/Widget";
+import PersonOutlinedIcon from "@mui/icons-material/PersonOutlined";
 
 import "./Home.scss";
+import {
+  AccountBalanceWalletOutlined,
+  MonetizationOnOutlined,
+  ShoppingCartOutlined,
+} from "@mui/icons-material";
 
 const DrawerHeader = styled("div")(({ theme }) => ({
   display: "flex",
@@ -73,10 +79,70 @@ export default function Home(props) {
           }}
           className="widgets"
         >
-          <Widget />
-          <Widget />
-          <Widget />
-          <Widget />
+          <Widget
+            type="USERS"
+            icon={
+              <PersonOutlinedIcon
+                className="icon"
+                style={{
+                  color: "crimson",
+                  backgroundColor: "rgba(255, 0, 0, 0.2)",
+                }}
+              />
+            }
+            link="View all users"
+            isMoney={false}
+            amount={1000}
+            percentage={30}
+          />
+          <Widget
+            type="ORDERS"
+            icon={
+              <ShoppingCartOutlined
+                className="icon"
+                style={{
+                  color: "goldenrod",
+                  backgroundColor: "rgba(218, 165, 32, 0.2)",
+                }}
+              />
+            }
+            link="View all orders"
+            isMoney={false}
+            amount={2000}
+            percentage={15}
+          />
+          <Widget
+            type="EARNINGS"
+            icon={
+              <MonetizationOnOutlined
+                className="icon"
+                style={{
+                  color: "green",
+                  backgroundColor: "rgba(0, 128, 0, 0.2)",
+                }}
+              />
+            }
+            link="View net earnings"
+            isMoney={true}
+            amount={23400}
+            percentage={-10}
+          />
+          <Widget
+            type="BALANCE"
+            icon={
+              <AccountBalanceWalletOutlined
+                className="icon"
+                style={{
+                  color: "purple",
+                  backgroundColor: "rgba(128, 0, 128, 0.2)",
+                }}
+              />
+            }
+            link="View details"
+            isMoney={true}
+            amount={50000}
+            percentage={0}
+          />
         </Box>
       </Box>
     </Box>
