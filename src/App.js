8e542a9c6ade2e076f6286c029f46f5756298678
aungkhant611658 <aungkhant611658@gmail.com>
2024-03-06 23:52:17 +0630

    setup sidebar and navbar

--- src/App.js
+++ src/App.js
@@ -1,32 +1,92 @@
-import Home from "./pages/home/Home";
-import Login from "./pages/login/Login";
-import List from "./pages/list/List";
-import Single from "./pages/single/Single";
 import New from "./pages/new/New";
-
+import Box from "@mui/material/Box";
+import Home from "./pages/home/Home";
+import List from "./pages/list/List";
+import Login from "./pages/login/Login";
+import Single from "./pages/single/Single";
+import { useState, useEffect } from "react";
+import Navbar from "./components/navbar/Navbar";
+import Sidebar from "./components/sidebar/Sidebar";
+import CssBaseline from "@mui/material/CssBaseline";
+import { styled, useTheme } from "@mui/material/styles";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-const App = () => (
-  <div className="App">
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="login" element={<Login />} />
+const DrawerHeader = styled("div")(({ theme }) => ({
+  display: "flex",
+  alignItems: "center",
+  justifyContent: "flex-end",
+  padding: theme.spacing(0, 1),
+  // necessary for content to be below app bar
+  ...theme.mixins.toolbar,
+}));
 
-        <Route path="users">
-          <Route index element={<List />} />
-          <Route path=":userId" element={<Single />} />
-          <Route path="new" element={<New />} />
-        </Route>
+const App = () => {
+  const theme = useTheme();
+  const [open, setOpen] = useState(true);
+  const [mobileMenu, setMobileMenu] = useState(false);
+  const [windowSize, setWindowSize] = useState([
+    window.innerWidth,
+    window.innerHeight,
+  ]);
 
-        <Route path="products">
-          <Route index element={<List />} />
-          <Route path=":productId" element={<Single />} />
-          <Route path="new" element={<New />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
-  </div>
-);
+  useEffect(() => {
+    const handleWindowResize = () => {
+      setWindowSize([window.innerWidth, window.innerHeight]);
+    };
+
+    window.addEventListener("resize", handleWindowResize);
+
+    return () => {
+      window.removeEventListener("resize", handleWindowResize);
+    };
+  }, []);
+
+  return (
+    <Box sx={{ display: "flex" }} className="App">
+      <CssBaseline />
+
+      <Navbar
+        open={open}
+        setOpen={setOpen}
+        windowSize={windowSize}
+        theme={theme}
+        mobileMenu={mobileMenu}
+        setMobileMenu={setMobileMenu}
+      />
+
+      <Sidebar
+        windowSize={windowSize}
+        mobileMenu={mobileMenu}
+        DrawerHeader={DrawerHeader}
+        setOpen={setOpen}
+        theme={theme}
+        open={open}
+      />
+
+      <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
+        <DrawerHeader />
+
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="login" element={<Login />} />
+
+            <Route path="users">
+              <Route index element={<List />} />
+              <Route path=":userId" element={<Single />} />
+              <Route path="new" element={<New />} />
+            </Route>
+
+            <Route path="products">
+              <Route index element={<List />} />
+              <Route path=":productId" element={<Single />} />
+              <Route path="new" element={<New />} />
+            </Route>
+          </Routes>
+        </BrowserRouter>
+      </Box>
+    </Box>
+  );
+};
 
 export default App;
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,10 +1,5 @@
 import * as React from "react";
-import { useState, useEffect } from "react";
-import { styled, useTheme } from "@mui/material/styles";
 import Box from "@mui/material/Box";
-import CssBaseline from "@mui/material/CssBaseline";
-import Navbar from "../../components/navbar/Navbar";
-import Sidebar from "../../components/sidebar/Sidebar";
 import Widget from "../../components/widget/Widget";
 import PersonOutlinedIcon from "@mui/icons-material/PersonOutlined";
 
@@ -15,136 +10,81 @@ import {
   ShoppingCartOutlined,
 } from "@mui/icons-material";
 
-const DrawerHeader = styled("div")(({ theme }) => ({
-  display: "flex",
-  alignItems: "center",
-  justifyContent: "flex-end",
-  padding: theme.spacing(0, 1),
-  // necessary for content to be below app bar
-  ...theme.mixins.toolbar,
-}));
-
-export default function Home(props) {
-  const theme = useTheme();
-  const [open, setOpen] = useState(true);
-  const [mobileMenu, setMobileMenu] = useState(false);
-  const [windowSize, setWindowSize] = useState([
-    window.innerWidth,
-    window.innerHeight,
-  ]);
-
-  useEffect(() => {
-    const handleWindowResize = () => {
-      setWindowSize([window.innerWidth, window.innerHeight]);
-    };
-
-    window.addEventListener("resize", handleWindowResize);
-
-    return () => {
-      window.removeEventListener("resize", handleWindowResize);
-    };
-  }, []);
-
+export default function Home() {
   return (
-    <Box sx={{ display: "flex" }}>
-      <CssBaseline />
-
-      <Navbar
-        open={open}
-        setOpen={setOpen}
-        windowSize={windowSize}
-        theme={theme}
-        mobileMenu={mobileMenu}
-        setMobileMenu={setMobileMenu}
+    <Box
+      sx={{
+        display: "flex",
+        flexDirection: "row",
+        flexWrap: "wrap",
+        justifyContent: "space-around",
+      }}
+      className="widgets"
+    >
+      <Widget
+        type="USERS"
+        icon={
+          <PersonOutlinedIcon
+            className="icon"
+            style={{
+              color: "crimson",
+              backgroundColor: "rgba(255, 0, 0, 0.2)",
+            }}
+          />
+        }
+        link="View all users"
+        isMoney={false}
+        amount={1000}
+        percentage={30}
       />
-
-      <Sidebar
-        windowSize={windowSize}
-        mobileMenu={mobileMenu}
-        DrawerHeader={DrawerHeader}
-        setOpen={setOpen}
-        theme={theme}
-        open={open}
+      <Widget
+        type="ORDERS"
+        icon={
+          <ShoppingCartOutlined
+            className="icon"
+            style={{
+              color: "goldenrod",
+              backgroundColor: "rgba(218, 165, 32, 0.2)",
+            }}
+          />
+        }
+        link="View all orders"
+        isMoney={false}
+        amount={2000}
+        percentage={15}
       />
-
-      <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
-        <DrawerHeader />
-
-        <Box
-          sx={{
-            display: "flex",
-            flexDirection: "row",
-            flexWrap: "wrap",
-            justifyContent: "space-around",
-          }}
-          className="widgets"
-        >
-          <Widget
-            type="USERS"
-            icon={
-              <PersonOutlinedIcon
-                className="icon"
-                style={{
-                  color: "crimson",
-                  backgroundColor: "rgba(255, 0, 0, 0.2)",
-                }}
-              />
-            }
-            link="View all users"
-            isMoney={false}
-            amount={1000}
-            percentage={30}
+      <Widget
+        type="EARNINGS"
+        icon={
+          <MonetizationOnOutlined
+            className="icon"
+            style={{
+              color: "green",
+              backgroundColor: "rgba(0, 128, 0, 0.2)",
+            }}
           />
-          <Widget
-            type="ORDERS"
-            icon={
-              <ShoppingCartOutlined
-                className="icon"
-                style={{
-                  color: "goldenrod",
-                  backgroundColor: "rgba(218, 165, 32, 0.2)",
-                }}
-              />
-            }
-            link="View all orders"
-            isMoney={false}
-            amount={2000}
-            percentage={15}
+        }
+        link="View net earnings"
+        isMoney={true}
+        amount={23400}
+        percentage={-10}
+      />
+      <Widget
+        type="BALANCE"
+        icon={
+          <AccountBalanceWalletOutlined
+            className="icon"
+            style={{
+              color: "purple",
+              backgroundColor: "rgba(128, 0, 128, 0.2)",
+            }}
           />
-          <Widget
-            type="EARNINGS"
-            icon={
-              <MonetizationOnOutlined
-                className="icon"
-                style={{
-                  color: "green",
-                  backgroundColor: "rgba(0, 128, 0, 0.2)",
-                }}
-              />
-            }
-            link="View net earnings"
-            isMoney={true}
-            amount={23400}
-            percentage={-10}
-          />
-          <Widget
-            type="BALANCE"
-            icon={
-              <AccountBalanceWalletOutlined
-                className="icon"
-                style={{
-                  color: "purple",
-                  backgroundColor: "rgba(128, 0, 128, 0.2)",
-                }}
-              />
-            }
-            link="View details"
-            isMoney={true}
-            amount={50000}
-            percentage={0}
-          />
-        </Box>
-      </Box>
+        }
+        link="View details"
+        isMoney={true}
+        amount={50000}
+        percentage={0}
+      />
     </Box>
   );
 }
