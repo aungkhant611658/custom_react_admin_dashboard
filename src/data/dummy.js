897fe5f24e9fa1830829d82497b8edec1c6c5942
aungkhant611658 <aungkhant611658@gmail.com>
2024-03-07 19:15:39 +0630

    setup chart

--- src/data/dummy.js
+++ src/data/dummy.js
@@ -10,6 +10,12 @@ import PsychologyOutlinedIcon from "@mui/icons-material/PsychologyOutlined";
 import SettingsApplicationsSharpIcon from "@mui/icons-material/SettingsApplicationsSharp";
 import AccountCircleOutlinedIcon from "@mui/icons-material/AccountCircleOutlined";
 import ExitToAppRoundedIcon from "@mui/icons-material/ExitToAppRounded";
+import PersonOutlinedIcon from "@mui/icons-material/PersonOutlined";
+import {
+  AccountBalanceWalletOutlined,
+  MonetizationOnOutlined,
+  ShoppingCartOutlined,
+} from "@mui/icons-material";
 
 export const sidebarMenu = [
   {
@@ -88,5 +94,72 @@ export const sidebarMenu = [
         name: "Logout",
       },
     ],
+  },
+];
+
+export const widgets = [
+  {
+    type: "USERS",
+    icon: (
+      <PersonOutlinedIcon
+        className="icon"
+        style={{
+          color: "crimson",
+          backgroundColor: "rgba(255, 0, 0, 0.2)",
+        }}
+      />
+    ),
+    link: "View all users",
+    isMoney: false,
+    amount: 1000,
+    percentage: 30,
+  },
+  {
+    type: "ORDERS",
+    icon: (
+      <ShoppingCartOutlined
+        className="icon"
+        style={{
+          color: "goldenrod",
+          backgroundColor: "rgba(218, 165, 32, 0.2)",
+        }}
+      />
+    ),
+    link: "View all orders",
+    isMoney: false,
+    amount: 2000,
+    percentage: 15,
+  },
+  {
+    type: "EARNINGS",
+    icon: (
+      <MonetizationOnOutlined
+        className="icon"
+        style={{
+          color: "green",
+          backgroundColor: "rgba(0, 128, 0, 0.2)",
+        }}
+      />
+    ),
+    link: "View net earnings",
+    isMoney: true,
+    amount: 23400,
+    percentage: -10,
+  },
+  {
+    type: "BALANCE",
+    icon: (
+      <AccountBalanceWalletOutlined
+        className="icon"
+        style={{
+          color: "purple",
+          backgroundColor: "rgba(128, 0, 128, 0.2)",
+        }}
+      />
+    ),
+    link: "View details",
+    isMoney: true,
+    amount: 50000,
+    percentage: 0,
   },
 ];
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,15 +1,10 @@
 import * as React from "react";
 import Box from "@mui/material/Box";
 import Widget from "../../components/widget/Widget";
-import PersonOutlinedIcon from "@mui/icons-material/PersonOutlined";
-import {
-  AccountBalanceWalletOutlined,
-  MonetizationOnOutlined,
-  ShoppingCartOutlined,
-} from "@mui/icons-material";
 import FeatureChart from "../../components/charts/featureChart/FeatureChart";
 import NormalChart from "../../components/charts/normalChart/NormalChart";
 import Grid from "@mui/material/Grid";
+import { widgets } from "../../data/dummy";
 
 import "./Home.scss";
 
@@ -25,78 +20,25 @@ export default function Home() {
         }}
         className="widgets"
       >
-        <Widget
-          type="USERS"
-          icon={
-            <PersonOutlinedIcon
-              className="icon"
-              style={{
-                color: "crimson",
-                backgroundColor: "rgba(255, 0, 0, 0.2)",
-              }}
-            />
-          }
-          link="View all users"
-          isMoney={false}
-          amount={1000}
-          percentage={30}
-        />
-        <Widget
-          type="ORDERS"
-          icon={
-            <ShoppingCartOutlined
-              className="icon"
-              style={{
-                color: "goldenrod",
-                backgroundColor: "rgba(218, 165, 32, 0.2)",
-              }}
-            />
-          }
-          link="View all orders"
-          isMoney={false}
-          amount={2000}
-          percentage={15}
-        />
-        <Widget
-          type="EARNINGS"
-          icon={
-            <MonetizationOnOutlined
-              className="icon"
-              style={{
-                color: "green",
-                backgroundColor: "rgba(0, 128, 0, 0.2)",
-              }}
-            />
-          }
-          link="View net earnings"
-          isMoney={true}
-          amount={23400}
-          percentage={-10}
-        />
-        <Widget
-          type="BALANCE"
-          icon={
-            <AccountBalanceWalletOutlined
-              className="icon"
-              style={{
-                color: "purple",
-                backgroundColor: "rgba(128, 0, 128, 0.2)",
-              }}
-            />
-          }
-          link="View details"
-          isMoney={true}
-          amount={50000}
-          percentage={0}
-        />
+        {widgets.map((widget) => (
+          <Widget
+            key={widget.type}
+            type={widget.type}
+            icon={widget.icon}
+            link={widget.link}
+            isMoney={widget.isMoney}
+            amount={widget.amount}
+            percentage={widget.percentage}
+          />
+        ))}
       </Box>
 
       <Box className="charts">
         <Grid container spacing={2}>
-          <Grid item xs={12} sm={12} md={10} lg={5}>
+          <Grid item xs={12} sm={12} md={10} lg={6}>
             <FeatureChart />
           </Grid>
-          <Grid item xs={12} sm={12} md={10} lg={7}>
+          <Grid item xs={12} sm={12} md={10} lg={6}>
             <NormalChart />
           </Grid>
         </Grid>
