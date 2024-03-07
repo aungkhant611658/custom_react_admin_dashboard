6b0b1377d73fbbc38c66e3027e2e7baa44c8cafd
aungkhant611658 <aungkhant611658@gmail.com>
2024-03-07 12:24:15 +0630

    setup home charts

---
+++ src/components/charts/NormalChart/NormalChart.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+import "./NormalChart.scss";
+
+const NormalChart = () => {
+  return <div>NormalChart</div>;
+};
+
+export default NormalChart;
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -12,79 +12,83 @@ import {
 
 export default function Home() {
   return (
-    <Box
-      sx={{
-        display: "flex",
-        flexDirection: "row",
-        flexWrap: "wrap",
-        justifyContent: "space-around",
-      }}
-      className="widgets"
-    >
-      <Widget
-        type="USERS"
-        icon={
-          <PersonOutlinedIcon
-            className="icon"
-            style={{
-              color: "crimson",
-              backgroundColor: "rgba(255, 0, 0, 0.2)",
-            }}
-          />
-        }
-        link="View all users"
-        isMoney={false}
-        amount={1000}
-        percentage={30}
-      />
-      <Widget
-        type="ORDERS"
-        icon={
-          <ShoppingCartOutlined
-            className="icon"
-            style={{
-              color: "goldenrod",
-              backgroundColor: "rgba(218, 165, 32, 0.2)",
-            }}
-          />
-        }
-        link="View all orders"
-        isMoney={false}
-        amount={2000}
-        percentage={15}
-      />
-      <Widget
-        type="EARNINGS"
-        icon={
-          <MonetizationOnOutlined
-            className="icon"
-            style={{
-              color: "green",
-              backgroundColor: "rgba(0, 128, 0, 0.2)",
-            }}
-          />
-        }
-        link="View net earnings"
-        isMoney={true}
-        amount={23400}
-        percentage={-10}
-      />
-      <Widget
-        type="BALANCE"
-        icon={
-          <AccountBalanceWalletOutlined
-            className="icon"
-            style={{
-              color: "purple",
-              backgroundColor: "rgba(128, 0, 128, 0.2)",
-            }}
-          />
-        }
-        link="View details"
-        isMoney={true}
-        amount={50000}
-        percentage={0}
-      />
-    </Box>
+    <>
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "row",
+          flexWrap: "wrap",
+          justifyContent: "space-around",
+        }}
+        className="widgets"
+      >
+        <Widget
+          type="USERS"
+          icon={
+            <PersonOutlinedIcon
+              className="icon"
+              style={{
+                color: "crimson",
+                backgroundColor: "rgba(255, 0, 0, 0.2)",
+              }}
+            />
+          }
+          link="View all users"
+          isMoney={false}
+          amount={1000}
+          percentage={30}
+        />
+        <Widget
+          type="ORDERS"
+          icon={
+            <ShoppingCartOutlined
+              className="icon"
+              style={{
+                color: "goldenrod",
+                backgroundColor: "rgba(218, 165, 32, 0.2)",
+              }}
+            />
+          }
+          link="View all orders"
+          isMoney={false}
+          amount={2000}
+          percentage={15}
+        />
+        <Widget
+          type="EARNINGS"
+          icon={
+            <MonetizationOnOutlined
+              className="icon"
+              style={{
+                color: "green",
+                backgroundColor: "rgba(0, 128, 0, 0.2)",
+              }}
+            />
+          }
+          link="View net earnings"
+          isMoney={true}
+          amount={23400}
+          percentage={-10}
+        />
+        <Widget
+          type="BALANCE"
+          icon={
+            <AccountBalanceWalletOutlined
+              className="icon"
+              style={{
+                color: "purple",
+                backgroundColor: "rgba(128, 0, 128, 0.2)",
+              }}
+            />
+          }
+          link="View details"
+          isMoney={true}
+          amount={50000}
+          percentage={0}
+        />
+      </Box>
+
+      <Box className="charts"></Box>
+    </>
   );
 }
